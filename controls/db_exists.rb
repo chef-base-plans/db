c4958da0ea1b848a38813231bffcf91aea8e6985
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 15:44:02 +0100

    Add description
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/db_exists.rb
+++ controls/db_exists.rb
@@ -7,7 +7,8 @@ control 'core-plans-db-exists' do
   impact 1.0
   title 'Ensure db exists'
   desc '
-  '
+  Verify db by ensuring /bin/db_dump exists'
+  
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
