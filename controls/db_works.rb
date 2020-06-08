9bafa17d792acdb74d060fcaa892989f83eedb22
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 15:44:02 +0100

    Add description
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/db_works.rb
+++ controls/db_works.rb
@@ -7,7 +7,10 @@ control 'core-plans-db-works' do
   impact 1.0
   title 'Ensure db works as expected'
   desc '
+  Verify db by ensuring (1) its installation directory exists and (2) that
+  it returns the expected version
   '
+  
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
